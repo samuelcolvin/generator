47c88c2a3283aa20c4e6d1d982b2c17fd731e7c5
Samuel Colvin <s@muelcolvin.com>
2015-11-11 15:28:42 +0000

    remove fake_storage

--- gen/api/wsgi.py
+++ gen/api/wsgi.py
@@ -6,11 +6,7 @@ from aiohttp import web
 BASE_DIR = os.path.abspath(os.path.join(__file__, '..', '..'))
 sys.path.append(BASE_DIR)
 
-from common import DEBUG
 from .views import APIController
 
 app = web.Application()
 APIController(app)
-if DEBUG:
-    from .fake_storage import FakeStorageController
-    FakeStorageController(app)
